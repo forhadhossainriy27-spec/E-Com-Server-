da9c2660f922045f25229f342d7b8a6368d4968a
Forhad <forhadhossainriyad27@gmail.com>
2026-07-07 02:16:08 +0600

    refactor(core): add standard response utility

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,18 +1,23 @@
 const asyncHandler = require("../middleware/asyncHandler");
 const { registerService } = require("../services/auth.service");
+const sendResponse = require("../utils/sendResponse");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { token, user } = await registerService(req.body);
 
-  res.status(201).json({
+  sendResponse({
+    res,
+    statusCode: 201,
     success: true,
     message: "User registered successfully.",
-    token,
-    user: {
-      id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
+    data: {
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
     },
   });
 });
---
+++ utils/sendResponse.js
@@ -0,0 +1,15 @@
+const sendResponse = ({
+  res,
+  statusCode = 200,
+  success = true,
+  message = "",
+  data = null,
+}) => {
+  res.status(statusCode).json({
+    success,
+    message,
+    data,
+  });
+};
+
+module.exports = sendResponse;
