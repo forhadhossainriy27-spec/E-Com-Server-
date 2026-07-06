1d9f6c30cff95814ef00c9c2818437acb5d85ed1
Forhad <forhadhossainriyad27@gmail.com>
2026-07-07 02:32:53 +0600

    feat(auth): implement http-only cookie authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,13 @@
 const asyncHandler = require("../middleware/asyncHandler");
 const { registerService } = require("../services/auth.service");
 const sendResponse = require("../utils/sendResponse");
+const setTokenCookie = require("../utils/setTokenCookie");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { token, user } = await registerService(req.body);
+
+    // Set HTTP-only cookie
+  setTokenCookie(res, token);
 
   sendResponse({
     res,
@@ -11,7 +15,6 @@ const registerUser = asyncHandler(async (req, res) => {
     success: true,
     message: "User registered successfully.",
     data: {
-      token,
       user: {
         id: user._id,
         name: user.name,
---
+++ utils/setTokenCookie.js
@@ -0,0 +1,10 @@
+const setTokenCookie = (res, token) => {
+  res.cookie("accessToken", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
+
+module.exports = setTokenCookie;
